c916885f89850a6ad719d258edc7ff48d46207f6
Carlo Munguia <carlomunguia@icloud.com>
2021-01-25 16:03:32 -0600

    building the User model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('desert plains')
+import { User } from './models/User'
+
+const user = new User({})
+
+user.set({ name: 'flower-0', age: 20 })
+
+console.log(user.get('name'))
+console.log(user.get('age'))
+
---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+interface UserProps {
+  name?: string
+  age?: number
+}
+
+type Callback = () => void
+
+export class User {
+  events: { [key: string]: Callback[] } = {}
+  constructor(private data: UserProps) { }
+
+  get(propName: string): (number | string) {
+    return this.data[propName]
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update)
+  }
+
+  on(eventName: string, callback: Callback): void {
+
+  }
+}
